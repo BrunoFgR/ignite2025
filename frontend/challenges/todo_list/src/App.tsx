a05f40074b61a8a97e023fa8c297c934ecac8d9f
brunoFgR <brunofds281@gmail.com>
2025-04-13 14:06:55 -0300

    create and style homepage

--- frontend/challenges/todo_list/src/App.tsx
+++ frontend/challenges/todo_list/src/App.tsx
@@ -1,10 +1,22 @@
 import Header from "./components/Header";
+import style from "./App.module.css";
+
 import "./global.css";
+import { PlusCircle } from "@phosphor-icons/react";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={style.wrapper}>
+        <form>
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+          <button type="submit">
+            Criar <PlusCircle size={16} />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
