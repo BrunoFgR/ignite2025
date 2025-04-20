123808ecdc68441aba8c940e1a8afce1559df479
brunoFgR <brunofds281@gmail.com>
2025-04-19 23:19:33 -0300

    Add phosphor-react and implement styled Header component

---
+++ frontend/classes/02-ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from './style'
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="Ignite Timer" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="History">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
