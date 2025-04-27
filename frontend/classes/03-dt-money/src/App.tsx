29ac584259dc444a3c4a2ed7203e832e26e7ab2b
brunoFgR <brunofds281@gmail.com>
2025-04-27 00:16:23 -0300

    feat: Add transaction table with styled components

--- frontend/classes/03-dt-money/src/App.tsx
+++ frontend/classes/03-dt-money/src/App.tsx
@@ -2,12 +2,40 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 import { Transactions } from "./pages/Transactions";
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionTable,
+} from "./pages/Transactions/styles";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
       <Transactions />
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>26/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Conta de energia</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 1.000,00</PriceHighlight>
+              </td>
+              <td>Conta</td>
+              <td>26/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </ThemeProvider>
   );
 }
