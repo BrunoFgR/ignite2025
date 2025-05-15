4b8dd48785d580608930a3081b53ee9c88fd2b26
brunoFgR <brunofds281@gmail.com>
2025-05-14 23:45:07 -0300

    feat: add AppLayout and AuthLayout components for routing structure

--- frontend/classes/04-pizzashop_web/src/routes.tsx
+++ frontend/classes/04-pizzashop_web/src/routes.tsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter } from "react-router";
 import { Dashboard } from "@/pages/app/Dashboard";
 import { SignIn } from "@/pages/auth/SignIn";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    Component: Dashboard,
+    Component: AppLayout,
+    children: [
+      {
+        path: "/",
+        Component: Dashboard,
+      },
+    ],
   },
   {
-    path: "sign-in",
-    Component: SignIn,
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "sign-in",
+        Component: SignIn,
+      },
+    ],
   },
 ]);
