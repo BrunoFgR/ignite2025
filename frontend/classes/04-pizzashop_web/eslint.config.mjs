a166019e08d4d39a109973bc740c4bd06b6fe08a
brunoFgR <brunofds281@gmail.com>
2025-05-14 22:57:25 -0300

    feat: update ESLint and Prettier configurations, add button component, and improve code structure

--- frontend/classes/04-pizzashop_web/eslint.config.mjs
+++ frontend/classes/04-pizzashop_web/eslint.config.mjs
@@ -3,15 +3,23 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
-
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
+export default defineConfig([
+  {
     extends: compat.extends("@rocketseat/eslint-config/react"),
-}]);
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+    },
+  },
+]);
--- frontend/classes/04-pizzashop_web/src/App.tsx
+++ frontend/classes/04-pizzashop_web/src/App.tsx
@@ -1,9 +1,10 @@
-import { Button } from './components/ui/button'
 import './global.css'
+
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800">Pizza Shop</h1>
       <p className="text-xl text-gray-600">Welcome to our pizza shop!</p>
       <Button>Enviar</Button>
