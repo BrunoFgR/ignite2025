02211c17ba37d5eaf4da8778535862efb4bc5023
brunoFgR <brunofds281@gmail.com>
2025-05-24 21:32:35 -0300

    feat: add axios and react-query setup, create App component with routing and environment configuration

--- frontend/classes/04-pizzashop_web/src/app.tsx
+++ frontend/classes/04-pizzashop_web/src/app.tsx
@@ -1,10 +1,12 @@
 import './global.css'
 
 import { Helmet, HelmetProvider } from '@dr.pogodin/react-helmet'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router'
 
 import { Toaster } from '@/components/ui/sonner'
 
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 import { ThemeProvider } from './theme/theme-provider'
 
@@ -13,7 +15,9 @@ export function App() {
     <HelmetProvider>
       <ThemeProvider storageKey="pizzashop-theme">
         <Helmet titleTemplate="%s | pizza.shop" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
         <Toaster richColors />
       </ThemeProvider>
     </HelmetProvider>
