46bcfc6b771937bc280aa513de613610bf367deb
brunoFgR <brunofds281@gmail.com>
2025-06-01 22:15:58 -0300

    feat: enhance AccountMenu with StoreProfileDialog and loading states; add success variant to Button; define Textarea component

--- frontend/classes/04-pizzashop_web/src/components/account-menu.tsx
+++ frontend/classes/04-pizzashop_web/src/components/account-menu.tsx
@@ -4,7 +4,9 @@ import { Building, ChevronDownIcon, LogOut, Settings } from 'lucide-react'
 import { getManagedRestaurant } from '@/api/get-managed-restaurant'
 import { getProfile } from '@/api/get-profile'
 
+import { StoreProfileDialog } from './store-profile-dialog'
 import { Button } from './ui/button'
+import { Dialog, DialogTrigger } from './ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -28,50 +30,56 @@ export function AccountMenu() {
     })
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="flex items-center gap-2 select-none"
-        >
-          {isManagedRestaurantLoading ? (
-            <Skeleton className="h-4 w-48" />
-          ) : (
-            managedRestaurant?.name
-          )}
-          <ChevronDownIcon className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          {isProfileLoading ? (
-            <div className="space-y-1.5">
-              <Skeleton className="h-4 w-32" />
-              <Skeleton className="h-3 w-24" />
-            </div>
-          ) : (
-            <>
-              <span>{profile?.name}</span>
-              <span className="text-muted-foreground text-xs font-normal">
-                {profile?.email}
-              </span>
-            </>
-          )}
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Building className="mr-2 h-4 w-4" />
-          Perfil da loja
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Settings className="mr-2 h-4 w-4" />
-          Configurações
-        </DropdownMenuItem>
-        <DropdownMenuItem className="black:text-rose-400 text-rose-500">
-          <LogOut className="mr-2 h-4 w-4" />
-          Sair
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="flex items-center gap-2 select-none"
+          >
+            {isManagedRestaurantLoading ? (
+              <Skeleton className="h-4 w-48" />
+            ) : (
+              managedRestaurant?.name
+            )}
+            <ChevronDownIcon className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isProfileLoading ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="text-muted-foreground text-xs font-normal">
+                  {profile?.email}
+                </span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="mr-2 h-4 w-4" />
+              Perfil da loja
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuItem>
+            <Settings className="mr-2 h-4 w-4" />
+            Configurações
+          </DropdownMenuItem>
+          <DropdownMenuItem className="black:text-rose-400 text-rose-500">
+            <LogOut className="mr-2 h-4 w-4" />
+            Sair
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog />
+    </Dialog>
   )
 }
---
+++ frontend/classes/04-pizzashop_web/src/components/store-profile-dialog.tsx
@@ -0,0 +1,88 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useQuery } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+
+import { Button } from './ui/button'
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './ui/dialog'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
+
+const storedProfileSchema = z.object({
+  name: z.string().min(2).max(100),
+  description: z.string().min(2).max(500),
+})
+
+type StoreProfileFormValues = z.infer<typeof storedProfileSchema>
+
+export function StoreProfileDialog() {
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+
+  const { register } = useForm<StoreProfileFormValues>({
+    resolver: zodResolver(storedProfileSchema),
+    defaultValues: {
+      name: managedRestaurant?.name || '',
+      description: managedRestaurant?.description || '',
+    },
+  })
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da Loja</DialogTitle>
+        <DialogDescription>
+          Atualize as informações do seu estabelecimento visíveis aos seus
+          clientes
+        </DialogDescription>
+      </DialogHeader>
+
+      <form>
+        <div className="gap-4 space-y-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">
+              Nome
+            </Label>
+            <Input
+              id="name"
+              type="text"
+              className="col-span-3"
+              {...register('name')}
+            />
+          </div>
+
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">
+              Descrição
+            </Label>
+            <Textarea
+              id="description"
+              className="col-span-3"
+              {...register('description')}
+            />
+          </div>
+        </div>
+
+        <DialogFooter>
+          <Button type="button" variant="ghost">
+            Cancelar
+          </Button>
+          <Button type="submit" variant="success">
+            Salvar
+          </Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  )
+}
